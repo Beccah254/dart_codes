062185b20974091944594ab43a66e26fe4e8734a
Beccah254 <rebeccahotoli7@gmail.com>
2024-03-07 21:11:34 +0300

    data_types file

---
+++ data_types.dart
@@ -0,0 +1,27 @@
+void main() {
+  // Integer data type
+  int myInt = 42;
+  print('Integer: $myInt');
+
+  // Double data type
+  double myDouble = 3.14;
+  print('Double: $myDouble');
+
+  // String data type
+  String myString = 'Hello, Dart!';
+  print('String: $myString');
+
+  // List data type
+  List<int> myList = [1, 2, 3, 4, 5];
+  print('List: $myList');
+
+  // Map data type
+  Map<String, int> myMap = {'apple': 5, 'banana': 10, 'orange': 7};
+  print('Map: $myMap');
+
+  // Testing List
+  print('First element of the list: ${myList[0]}');
+
+  // Testing Map
+  print('Number of apples: ${myMap['apple']}');
+}
